cac3f5ddfb7cdd7d3ecdc97dd7434e354bd71b01
cs8425 <cs8425@gmail.com>
2013-10-12 02:39:10 +0800

    第4堂作業

--- src/com/example/l2/MainActivity.java
+++ src/com/example/l2/MainActivity.java
@@ -8,15 +8,41 @@ import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivity extends Activity {
+	static String[] item = new String[]{"磅","盎司"};
+	int select = 0;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		
+		Spinner spinner = (Spinner)findViewById(R.id.spinner1);
+		ArrayAdapter<String> adp = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_dropdown_item, item);
+
+		adp.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+		spinner.setAdapter(adp);
+		spinner.setOnItemSelectedListener(new Spinner.OnItemSelectedListener(){
+			@Override
+			public void onItemSelected(AdapterView<?> arg0, View arg1,
+					int arg2, long arg3) {
+				// TODO Auto-generated method stub
+				Toast.makeText(MainActivity.this, String.valueOf(arg2)+","+String.valueOf(arg3) , Toast.LENGTH_SHORT).show();
+				select = arg2;
+			}
+
+			@Override
+			public void onNothingSelected(AdapterView<?> arg0) {
+				// TODO Auto-generated method stub
+				Toast.makeText(MainActivity.this, "請選一項!!", Toast.LENGTH_SHORT).show();
+			}
+		});
 	}
 
 	@Override
@@ -45,8 +71,13 @@ public class MainActivity extends Activity {
 		if(in.length() != 0) {
 			try{
 				kg = Double.parseDouble(in.toString());
-				double pound = kg * 2.20462262;
-				((TextView)findViewById(R.id.output)).setText(String.valueOf(pound));
+				double out;
+				if(select == 0){
+					out = kg * 2.20462262;
+				}else{
+					out = kg * 35.2739619;
+				}
+				((TextView)findViewById(R.id.output)).setText(String.valueOf(out));
 				//view.findViewById(R.id.editText2)
 				//onAbout();
 			}
@@ -65,6 +96,6 @@ public class MainActivity extends Activity {
 				// TODO Auto-generated method stub
 			}
 		};
-		new AlertDialog.Builder(MainActivity.this).setTitle("關於L2").setMessage("第2堂\n測試用\n").setPositiveButton("OK",OK).show();
+		new AlertDialog.Builder(MainActivity.this).setTitle("關於L4").setMessage("第4堂\n測試用\n").setPositiveButton("OK",OK).show();
 	}
 }
